77c6479feec23961dfffa3a9fa0096aa76d787ea
Sunakshi <sunakshiisharma0060@gmail.com>
2026-08-20 23:00:28 +0530

    Review3 Ques

---
+++ ReviewTestProject/ReviewQues/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a single value: ");
+        string input = Console.ReadLine();
+
+        if (Regex.IsMatch(input, "^[a-zA-Z]$"))
+        {
+            Console.WriteLine("The entered value is a Character.");
+        }
+        else if (Regex.IsMatch(input, "^[0-9]$"))
+        {
+            Console.WriteLine("The entered value is an Integer.");
+        }
+        else if(Regex.IsMatch(input, @"^-?\d+$"))
+        {
+            Console.WriteLine("The entered value is a Special Symbol.");
+        }
+        else
+        {
+            Console.WriteLine("Input is Empty.");
+        }
+
+        Console.ReadKey();
+    }
+}
